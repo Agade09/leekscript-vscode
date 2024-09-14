5f1a0d9517f040638e64676dc611de4291179496
Agade09 <n.sillitoe@gmail.com>
2024-09-14 14:01:31 +0200

    Use https://leekwars.com/api/function/get-all to get all builtin function names

--- tools/BuiltinFunctions_Extraction.py
+++ tools/BuiltinFunctions_Extraction.py
@@ -1,14 +1,16 @@
 import requests
-import re
 
-url = "https://raw.githubusercontent.com/leek-wars/leek-wars/0580d239bc162111ae460198a2e3eed3cc2c4db6/src/model/functions.ts"
+url = "https://leekwars.com/api/function/get-all"
 
 response = requests.get(url)
 
 if response.status_code == 200:
-    data = response.text
-    names = re.findall(r"\bname: '([^']*)'", data)
-    print("|".join(names))
-    print("Found", len(names), "functions")
+    data = response.json()
+    names = "|".join([function["name"] for function in data["functions"]])
+    print(names)
+    print("Found", len(data["functions"]), "functions")
+    with open("builtin_functions_highlighting.txt", "w") as file:
+        file.write(names)
 else:
     print("Failed to download file. Status code:", response.status_code)
+
